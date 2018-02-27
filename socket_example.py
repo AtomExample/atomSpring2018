9ced9e1a1553434691766f7d8804cbe223701483
Sergey Lihobabin <greyone@greyone.ru>
2018-02-27 10:33:58 -0500

    Initial commit

---
+++ socket_example.py
@@ -0,0 +1,32 @@
+import socket
+
+HOSTNAME = '127.0.0.1'
+PORT = 2000
+CONNECTION_COUNT = 1
+MSG_LENGTH = 1024
+
+serversocket = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM
+)
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+serversocket.bind((HOSTNAME, PORT))
+serversocket.listen(CONNECTION_COUNT)
+
+RESPONSE_TEMPLATE = 'HTTP/1.1 200 OK\nConnection: close\n\n{response_body}\n'
+
+requests_counter = 0
+
+print 'Listening on {0} port {1}'.format(HOSTNAME, PORT)
+while True:
+    clientsocket, address = serversocket.accept()
+    print clientsocket.recv(MSG_LENGTH)
+    requests_counter += 1
+    clientsocket.send(
+        RESPONSE_TEMPLATE.format(response_body=requests_counter)
+    )
+    clientsocket.shutdown(1)
+
+    clientsocket.close()
+
+serversocket.close()
